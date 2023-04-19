56ba23cf94435895cf2065a18c4bd62be172a611
Anamika Nayak <anayak@pdx.edu>
2023-04-18 18:36:01 -0700

    Initial backend configuration

---
+++ backend/src/index.ts
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import Fastify, {FastifyReply, FastifyRequest} from "fastify";
+import {User} from "./db/entities/User.js";
+import config from "./db/mikro-orm.config.js";
+import {FastifyMikroOrmPlugin} from "./plugins/mikro.js";
+
+dotenv.config();
+
+const app = Fastify();
+
+await app.register(FastifyMikroOrmPlugin, config);
+
+app.get('/hello', async (request: FastifyRequest, reply: FastifyReply) => {
+	return 'hello';
+}); 
+
+app.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
+	return 'Hilo';
+}); 
+
+app.get("/dbTest", async (request: FastifyRequest, reply: FastifyReply) => {
+	return request.em.find(User, {});
+});
+
+app.listen({ port: process.env.PORT}, 
+	(err, address) => {
+		if (err) {
+			console.error(err);
+			process.exit(1);
+		}
+		console.log(`Started server at ${address}`);
+	}
+);
