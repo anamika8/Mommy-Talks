13985ff17c9130ccadaf209574a7325958433973
Anamika Nayak <anayak@pdx.edu>
2023-05-14 20:49:44 -0700

    added signup and login page button

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,26 +1,7 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import axios from "axios";
-
-
-export const Home = () => {
-    return (
-        <div>
-            <Title />
-            <Subtitle />
-            <UsersList />
-
-        </div>
-    );
-};
-
-export function Title() {
-    return(<h1>Mommy Talks</h1>);
-}
-
-export function Subtitle() {
-    return(<h3>Moms will not feel alone here!!!</h3>);
-}
-
+import {Link} from "react-router-dom";
 
 // 1) Make a place to store the users list result
 // 2) Make the actual request to backend and store result
@@ -34,8 +15,60 @@ export function Subtitle() {
  * You “use” React features at the top of your component similar to how you “import” modules
  * at the top of your file.
  */
+export const Home = () => {
+    return (
+        <div>
+            <Title />
+            <Subtitle />
+            <Main />
+            <LoginButton />
+            <SignupButton />
+        </div>
+    );
+};
+
+export function Title() {
+    return(<h1>Mommy Talks</h1>);
+}
+
+export function Subtitle() {
+    return(<h3>Moms will not feel alone here!!!</h3>);
+}
+
+export function Main() {
+    return(<p className="theme-details">A place for new, upcoming and experienced mothers to share their concern, get
+        helpful suggestions.</p>
+    );
+}
+
+const LoginButton = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        // Navigate to a different page
+        navigate('/Match');
+    };
+
+    return (
+        <button onClick={handleClick}>Login</button>
+    );
+};
+
+const SignupButton = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        // Navigate to a different page
+        navigate('/Home');
+    };
+
+    return (
+        <button onClick={handleClick}>Signup</button>
+    );
+};
 export const Button = () => {
     const [clicks, setClicks] = useState(0);
+    //const history = useHistory();
 
     return (
         <button
