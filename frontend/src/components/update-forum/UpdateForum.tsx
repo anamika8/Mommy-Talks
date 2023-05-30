596433d853c2f4065abe9943f0b6989d2e2f5c60
Anamika Nayak <anayak@pdx.edu>
2023-05-29 17:30:34 -0700

    added update forum page

--- frontend/src/components/update-forum/UpdateForum.tsx
+++ frontend/src/components/update-forum/UpdateForum.tsx
@@ -1,11 +1,129 @@
-import React from 'react';
-import { BrowserRouter as Router, Link, useNavigate } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 import symbol from "@/components/forum/logo.jpg";
+import "./update-forum.css";
 export const UpdateForum = () => {
     return (
         <div>
             <Header />
+            <MainComponent />
+            <FontawesomeScript />
         </div>
+    );
+};
+export const MainComponent = () => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [comment, setComment] = useState('');
+    const [submitFailed, setSubmitFailed] = useState(false);
+
+    const handleTitleChange = (event) => {
+        if (!submitFailed) {
+            setTitle(event.target.value);
+        }
+    };
+
+    const handleContentChange = (event) => {
+        if (!submitFailed) {
+            setContent(event.target.value);
+        }
+    };
+    const handleUpdate = (event) => {
+        // Handle update logic here
+        event.preventDefault();
+        console.log("Submitted:", title, content);
+
+        setTitle("");
+        setContent("");
+        setSubmitFailed(true);
+
+    };
+
+    const handleDelete = (event) => {
+        // Handle delete logic here
+        event.preventDefault();
+        console.log("Deleted");
+    };
+
+    const handleCommentChange = (event) => {
+        setComment(event.target.value);
+    };
+
+    const handleCommentChangeSubmit = (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+
+            // Handle comment submission logic here
+            // e.g., Post the comment to the server
+
+            // Reset the comment input
+            setComment('');
+
+            // Log the comment
+            console.log('Comment posted:', comment);
+        }
+    };
+
+    return (
+        <main role="main">
+            <form action="/query" method="post" id="query-form">
+                <p>
+                    <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        placeholder="Title"
+                        onChange={handleTitleChange}
+                        value={title}
+                    />
+                </p>
+                <div id="content-section" className="colm-4">
+                    <p>
+                        <textarea
+                            name="content"
+                            id="mt-editor"
+                            placeholder="Content"
+                            onChange={handleContentChange}
+                            value={content}>
+                        </textarea>
+                    </p>
+                    <span id="user-info">
+                        <i className="fa fa-user-circle-o" aria-hidden="true"></i> User: Test Mommy
+                    </span>
+                </div>
+                <br />
+                <div id="outer">
+                    <div className="inner">
+                        <button type="button" name="submit" id="updatePost" onClick={handleUpdate}>
+                            Update
+                        </button>
+                    </div>
+                    <div className="inner">
+                        <button type="button" name="submit" id="deletePost" onClick={handleDelete}>
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+            <div className="rows" id="all-comment">
+                <div className="comment_log colm-4 border" id="defaultCommentBox">
+                    <input
+                        type="text"
+                        name="comment"
+                        className="colm-4"
+                        id="newComment"
+                        placeholder="Write your comment"
+                        onChange={handleCommentChange}
+                        value={comment}
+                        onKeyDown={handleCommentChangeSubmit}
+                    />
+                    <span className="user">
+                        <i className="fa fa-user-circle-o" aria-hidden="true"></i> User: Test Mommy
+                    </span>
+                </div>
+            </div>
+        </main>
     );
 };
 
@@ -60,4 +178,18 @@ export const Header = () => {
             </div>
         </header>
     );
+};
+const FontawesomeScript = () => {
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://use.fontawesome.com/3a36aded45.js';
+        script.async = true;
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    return null;
 };
