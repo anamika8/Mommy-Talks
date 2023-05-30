7c98a334aa7a11e019e3b4fa3ee5c47b3abe220b
Anamika Nayak <anayak@pdx.edu>
2023-05-29 17:46:49 -0700

    added more optimised UpdateForum Page

--- frontend/src/components/update-forum/UpdateForum.tsx
+++ frontend/src/components/update-forum/UpdateForum.tsx
@@ -12,9 +12,9 @@ export const UpdateForum = () => {
     );
 };
 
-const Form = ({ handleUpdate, handleDelete, handleTitleChange, handleContentChange, title, content }) => {
+const Form = ({ handleSubmit, handleTitleChange, handleContentChange, title, content }) => {
     return (
-        <form action="/query" method="post" id="query-form">
+        <form onSubmit={handleSubmit} id="query-form">
             <p>
                 <input
                     type="text"
@@ -25,29 +25,29 @@ const Form = ({ handleUpdate, handleDelete, handleTitleChange, handleContentChan
                     value={title}
                 />
             </p>
-            <div id="content-section" className="colm-4">
+            <div className="colm-4" id="content-section">
                 <p>
           <textarea
               name="content"
               id="mt-editor"
               placeholder="Content"
               onChange={handleContentChange}
-              value={content}>
-          </textarea>
+              value={content}
+          />
                 </p>
                 <span id="user-info">
-          <i className="fa fa-user-circle-o" aria-hidden="true"></i> User: Test Mommy
+          <i className="fa fa-user-circle-o" aria-hidden="true" /> User: Test Mommy
         </span>
             </div>
             <br />
             <div id="outer">
                 <div className="inner">
-                    <button type="button" name="submit" id="updatePost" onClick={handleUpdate}>
+                    <button type="button" name="submit" id="updatePost">
                         Update
                     </button>
                 </div>
                 <div className="inner">
-                    <button type="button" name="submit" id="deletePost" onClick={handleDelete}>
+                    <button type="button" name="submit" id="deletePost">
                         Delete
                     </button>
                 </div>
@@ -56,9 +56,9 @@ const Form = ({ handleUpdate, handleDelete, handleTitleChange, handleContentChan
     );
 };
 
-const Comment = ({ handleCommentChange, handleCommentChangeSubmit, comment }) => {
+const Comment = ({ handleCommentChange, handleCommentSubmit, comment }) => {
     return (
-        <div className="rows" id="all-comment">
+        <div id="all-comment">
             <div className="comment_log colm-4 border" id="defaultCommentBox">
                 <input
                     type="text"
@@ -68,10 +68,10 @@ const Comment = ({ handleCommentChange, handleCommentChangeSubmit, comment }) =>
                     placeholder="Write your comment"
                     onChange={handleCommentChange}
                     value={comment}
-                    onKeyDown={handleCommentChangeSubmit}
+                    onKeyDown={handleCommentSubmit}
                 />
                 <span className="user">
-          <i className="fa fa-user-circle-o" aria-hidden="true"></i> User: Test Mommy
+          <i className="fa fa-user-circle-o" aria-hidden="true" /> User: Test Mommy
         </span>
             </div>
         </div>
@@ -82,51 +82,35 @@ export const MainComponent = () => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const [comment, setComment] = useState('');
-    const [submitFailed, setSubmitFailed] = useState(false);
 
     const handleTitleChange = (event) => {
-        if (!submitFailed) {
-            setTitle(event.target.value);
-        }
+        setTitle(event.target.value);
     };
 
     const handleContentChange = (event) => {
-        if (!submitFailed) {
-            setContent(event.target.value);
-        }
+        setContent(event.target.value);
     };
+
     const handleUpdate = (event) => {
-        // Handle update logic here
         event.preventDefault();
-        console.log("Submitted:", title, content);
-
-        setTitle("");
-        setContent("");
-        setSubmitFailed(true);
-
+        console.log('Submitted:', title, content);
+        setTitle('');
+        setContent('');
     };
 
     const handleDelete = (event) => {
-        // Handle delete logic here
         event.preventDefault();
-        console.log("Deleted");
+        console.log('Deleted');
     };
 
     const handleCommentChange = (event) => {
         setComment(event.target.value);
     };
 
-    const handleCommentChangeSubmit = (event) => {
+    const handleCommentSubmit = (event) => {
         if (event.key === 'Enter') {
             event.preventDefault();
-
-            // Handle comment submission logic here
-            // e.g., Post the comment to the server
-
-            // Reset the comment input
             setComment('');
-
-            // Log the comment
             console.log('Comment posted:', comment);
         }
     };
@@ -134,8 +118,7 @@ export const MainComponent = () => {
     return (
         <main role="main">
             <Form
-                handleUpdate={handleUpdate}
-                handleDelete={handleDelete}
+                handleSubmit={handleUpdate}
                 handleTitleChange={handleTitleChange}
                 handleContentChange={handleContentChange}
                 title={title}
@@ -143,7 +126,7 @@ export const MainComponent = () => {
             />
             <Comment
                 handleCommentChange={handleCommentChange}
-                handleCommentChangeSubmit={handleCommentChangeSubmit}
+                handleCommentSubmit={handleCommentSubmit}
                 comment={comment}
             />
         </main>
