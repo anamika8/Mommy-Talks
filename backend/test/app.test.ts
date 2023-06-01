6b1b599ea4eef3c6baea6a5fe7869f37e935fc49
Anamika Nayak <anayak@pdx.edu>
2023-06-01 13:33:54 -0700

    added test cases for user

--- backend/test/app.test.ts
+++ backend/test/app.test.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck
-/*import * as dotenv from "dotenv";
+import * as dotenv from "dotenv";
 
 dotenv.config();
 import "chai/register-should.js"; // Using Should style
@@ -15,203 +15,101 @@ import { DatabaseSeeder } from "../src/db/seeders/DatabaseSeeder.js";
 let orm: MikroORM;
 
 tap.before(async () => {
-	app.log.warn("Initializing database...");
-	orm = await MikroORM.init(config);
-	const seeder: ISeedManager = orm.getSeeder();
-	app.log.warn("Refreshing database schema...");
-	await orm.getSchemaGenerator().refreshDatabase();
-	app.log.warn("Database refreshed, seeding...");
-	await seeder.seed(DatabaseSeeder);
-	app.log.warn("Finished seeding.");
+    app.log.warn("Initializing database...");
+    orm = await MikroORM.init(config);
+    const seeder: ISeedManager = orm.getSeeder();
+    app.log.warn("Refreshing database schema...");
+    await orm.getSchemaGenerator().refreshDatabase();
+    app.log.warn("Database refreshed, seeding...");
+    await seeder.seed(DatabaseSeeder);
+    app.log.warn("Finished seeding.");
 });
 
 tap.teardown(async () => {
-	await orm.close();
-	await app.close();
+    await orm.close();
+    await app.close();
 });
 
-void tap.test("List all users from /dbvoid tap.test", async () => {
-	const response = await app.inject({
-		method: "GET",
-		url: "/dbTest",
-	});
+void tap.test("List all users from /dbTest", async () => {
+    const response = await app.inject({
+        method: "GET",
+        url: "/dbTest",
+    });
 
-	response.statusCode.should.equal(200);
+    response.statusCode.should.equal(200);
+});
+//User routes testing
+void tap.test("GET /users should return all users", async () => {
+    const response = await app.inject({
+        method: "GET",
+        url: "/users",
+    });
+
+    response.statusCode.should.equal(200);
+
+    const users = JSON.parse(response.body);
+    Array.isArray(users).should.equal(true);
 });
 
-void tap.test("Creating a new user", async () => {
-	const payload = {
-		name: "void tap.testname",
-		email: faker.internet.email(),
-		password: "password",
-		role: UserRole.USER,
-		petType: "Dog",
-	};
+void tap.test("POST /users should create a new user", async () => {
+    const user = {
+        first_name: "John",
+        last_name: "Doe",
+        email: "john.doe@example.com",
+        password: "password",
+    };
 
-	const response = await app.inject({
-		method: "POST",
-		url: "/users",
-		payload,
-	});
+    const response = await app.inject({
+        method: "POST",
+        url: "/users",
+        payload: user,
+    });
 
-	response.statusCode.should.equal(200);
-	response.payload.should.not.equal(payload);
-	const resPayload = response.json();
-	resPayload.email.should.equal(payload.email);
-	resPayload.petType.should.equal("Dog");
+    response.statusCode.should.equal(200);
+
+    const newUser = JSON.parse(response.body);
+    newUser.first_name.should.equal(user.first_name);
+    newUser.last_name.should.equal(user.last_name);
+    newUser.email.should.equal(user.email);
 });
 
-void tap.test("Creating a new message", async () => {
-	const payload = {
-		sender_id: 1,
-		receiver_id: 3,
-		message: "Hi",
-	};
+void tap.test("PUT /users should update a user", async () => {
+    // Assuming there is a user with ID 1
+    const userId = 1;
+    const updatedUser = {
+        id: userId,
+        first_name: "Updated",
+        last_name: "User",
+    };
 
-	const response = await app.inject({
-		method: "POST",
-		url: "/messages",
-		payload,
-	});
+    const response = await app.inject({
+        method: "PUT",
+        url: "/users",
+        payload: updatedUser,
+    });
 
-	response.statusCode.should.equal(200);
-	response.payload.should.not.equal(payload);
-	const resPayload = response.json();
-	resPayload.message.should.equal(payload.message);
+    response.statusCode.should.equal(200);
+
+    const user = JSON.parse(response.body);
+    user.id.should.equal(userId);
+    user.first_name.should.equal(updatedUser.first_name);
+    user.last_name.should.equal(updatedUser.last_name);
 });
 
-void tap.test("Reading messages sent to a specific user", async () => {
-	const payload = {
-		receiver_id: 3,
-	};
+void tap.test("GET /users with ID should return a specific user", async () => {
+    // Assuming there is a user with ID 1
+    const userId = 1;
 
-	const response = await app.inject({
-		method: "SEARCH",
-		url: "/messages/received",
-		payload,
-	});
+    const response = await app.inject({
+        method: "GET",
+        url: `/users?id=${userId}`,
+    });
 
-	response.statusCode.should.equal(200);
+    response.statusCode.should.equal(200);
+
+    const user = JSON.parse(response.body);
+    console.log(user);
+    const foundUser = user.find((u) => u.id === userId);
+    foundUser.should.exist;
+    foundUser.id.should.equal(userId);
 });
-
-void tap.test("Reading messages sent BY a specific user", async () => {
-	const payload = {
-		sender_id: 1,
-	};
-
-	const response = await app.inject({
-		method: "SEARCH",
-		url: "/messages/sent",
-		payload,
-	});
-
-	response.statusCode.should.equal(200);
-});
-
-void tap.test("Updating a sent message", async () => {
-	const payload = {
-		message_id: 1,
-		message: "New message text",
-	};
-
-	const response = await app.inject({
-		method: "PUT",
-		url: "/messages",
-		payload,
-	});
-
-	response.statusCode.should.equal(200);
-	const resPayload = response.json();
-	resPayload.message.should.equal(payload.message);
-});
-
-void tap.test("Deleting a specific message", async () => {
-	let payload = {
-		my_id: 1,
-		message_id: 5,
-		password: "password",
-	};
-
-	let response = await app.inject({
-		method: "DELETE",
-		url: "/messages",
-		payload,
-	});
-
-	response.statusCode.should.equal(200);
-
-	// ensure to check that my_id is validity checked
-	payload = { ...payload, my_id: 1000000 };
-
-	response = await app.inject({
-		method: "DELETE",
-		url: "/messages",
-		payload,
-	});
-
-	response.statusCode.should.equal(500);
-
-	// ensure to check that "bad" passwords fail, too!
-	payload = { ...payload, my_id: 1, password: "password2" };
-
-	response = await app.inject({
-		method: "DELETE",
-		url: "/messages",
-		payload,
-	});
-
-	response.statusCode.should.equal(401);
-});
-
-void tap.test("Deleting all sent messages", async () => {
-	const payload = {
-		my_id: 1,
-		password: "password",
-	};
-
-	const response = await app.inject({
-		method: "DELETE",
-		url: "/messages/all",
-		payload,
-	});
-
-	response.statusCode.should.equal(200);
-});
-
-void tap.test("Deleting all sent messages fails with incorrect password", async () => {
-	const payload = {
-		my_id: 3,
-		password: "WRONG",
-	};
-
-	const response = await app.inject({
-		method: "DELETE",
-		url: "/messages/all",
-		payload,
-	});
-
-	console.log(response.payload);
-
-	response.statusCode.should.equal(401);
-});
-
-void tap.test("Testing message bad words filter", async () => {
-	const payload = {
-		sender_id: 1,
-		receiver_id: 2,
-		message: "Hi you shit",
-	};
-
-	const response = await app.inject({
-		method: "POST",
-		url: "/messages",
-		payload,
-	});
-
-	response.statusCode.should.equal(500);
-	response.payload.should.not.equal(payload);
-	const resPayload = response.json();
-	resPayload.message.should.equal("Bad words naughty list added.");
-	resPayload.message.should.not.equal("Bad words naughty list added!");
-});
-*/
